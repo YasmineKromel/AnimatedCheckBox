03438a6a8168605d26d9289d042ffe937c245bb9
yasmine Soubhi kromel <yasmine@appstudio.pro>
2019-06-16 16:14:32 +0200

    committed and pushed

--- AnimatedCheckbox/ViewController.swift
+++ AnimatedCheckbox/ViewController.swift
@@ -14,7 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
-
+    @IBAction func checkedTapped(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations:{ sender.transform = CGAffineTransform(scaleX: 0.1, y:0.1)}) { (success) in
+            sender.isSelected = !sender.isSelected
+       
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.transform = .identity
+            }, completion: nil)
+        }
+        
+//        if sender.isSelected
+//        {
+//            sender.isSelected = false
+//        }
+//        else{
+//            sender.isSelected = true
+//        }
+    }
+    
 }
 
